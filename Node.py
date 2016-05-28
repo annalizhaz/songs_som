4d7a867a621294ee3b8a838941ae618ee88c9b8c
capp30254 <capp30254@capp30254-VirtualBox-64.(none)>
2016-05-28 03:31:14 -0500

    added 2 lines of code

--- Node.py
+++ Node.py
@@ -4,4 +4,11 @@ class Node:
     def __init__(self, x, y, n):
         self.x = x
         self.y = y
-        self.weights = [random.random() for x in range(n)]
+        self.n = n
+        self.weights = [random.random() for x in range(self.n)]
+
+    def calculate_distance(input_vector):
+        sum([(attrb - self.weights[i])^2 for i, attrb in enumerate(input_vector)])
+
+    def set_weights(weights):
+        self.weights = weights
