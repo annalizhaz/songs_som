32d4b73fd8eb1fa9a340e3412ad5416d46c89652
capp30254 <capp30254@capp30254-VirtualBox-64.(none)>
2016-05-28 02:44:25 -0500

    redo

--- SOM.py
+++ SOM.py
@@ -1,3 +1,6 @@
 class SOM:
-    def __init__(self, x, y, n):
-        self.map = [[Node(i, j, n) of i in range(x)] for j in range(y)]
+    def __init__(self, x_len, y_len, n):
+        self.x_len = x
+        self.y_len = y
+        self.n = n
+        self.map = [[Node(i, j, self.n) for i in range(self.x_len)] for j in range(self.y_len)]
