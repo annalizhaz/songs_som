d685be69de9d757776f128bc7f899e1ccc90be15
capp30254 <capp30254@capp30254-VirtualBox-64.(none)>
2016-05-28 05:33:33 -0500

    implementation without mapreduce

--- Node.py
+++ Node.py
@@ -6,9 +6,20 @@ class Node:
         self.y = y
         self.n = n
         self.weights = [random.random() for x in range(self.n)]
+        self.weight_num = 0
+        self.weight_dem = 0
 
-    def calculate_distance(input_vector):
+    def calculate_distance(self, input_vector):
         return sum([(attrb - self.weights[i])^2 for i, attrb in enumerate(input_vector)])
 
-    def set_weights(weights):
-        self.weights = weights
+    def update_weights(self, weights):
+        self.weights = self.weight_num / self.weight_dem
+        ## Clear weight ratios
+        self.weight_num, self.weight_dem = 0
+
+    def clear_weight_ratio(self):
+        self.weight_num, self.weight_dem = 0
+
+    def update_weight_ratio(self, weight_num, weight_dem):
+        self.weight_num = weight_num + self.weight_num
+        self.weight_dem = weight_dem + self.weight_dem
