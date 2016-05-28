8e5250ec16d6c1e87f3c369b9de574a2f19527d7
annalizhaz <annalizhaz@gmail.com>
2016-05-28 16:10:12 -0500

    some really important stuff

--- SOM.py
+++ SOM.py
@@ -1,6 +1,23 @@
+import random
+import 
+
 class SOM:
-    def __init__(self, x_len, y_len, n):
-        self.x_len = x
-        self.y_len = y
+    def __init__(self, n, x_len = 10, y_len = 10, norm = 1, epochs = 1000):
+    	'''
+    	x_len, y_len: size of grid
+    	n: size of vectors (number of attributes in data)
+    	'''
+        self.x_len = x_len
+        self.y_len = y_len
         self.n = n
         self.map = [[Node(i, j, self.n) for i in range(self.x_len)] for j in range(self.y_len)]
+        self.norm = norm
+        self.epochs = epochs
+
+    def train_map(self):
+    	
+
+
+
+
+
