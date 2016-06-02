413c34cd8383fd0a97a8964285a48ed434d2fade
capp30254 <capp30254@capp30254-VirtualBox-64.(none)>
2016-06-02 10:14:45 -0500

    v. messy implementation of graphing

--- SOM.py
+++ SOM.py
@@ -5,6 +5,10 @@ from UMatrixMapper import UMatrixMapper
 import sys
 import csv
 
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+
 class SOM:
     def __init__(self, n, x_len = 10, y_len = 10, epochs = 10, theta_naught = 10, theta_f = .2):
         '''
@@ -61,10 +65,11 @@ class SOM:
         for line in runner.stream_output():
             (x, y), value = job.parse_output_line(line)
             u_matrix[x][y] = value
+        return np.array(u_matrix)
 
         with open("u_matrix.txt", "w") as file_name:
-                writer = csv.writer(file_name)
-                writer.writerows(u_matrix, delimiter = " ")
+                writer = csv.writer(file_name, delimiter = " ")
+                writer.writerows(u_matrix)
 
     def get_u_matrix(self):
         self.write_nodes_to_file()
@@ -73,9 +78,73 @@ class SOM:
 
         with compute_u_matrix_job.make_runner() as compute_u_matrix_runner:
             compute_u_matrix_runner.run()
-            self.extract_u_matrix(compute_u_matrix_job, compute_u_matrix_runner)
+            matrix = self.extract_u_matrix(compute_u_matrix_job, compute_u_matrix_runner)
+            return matrix
+
+    def u_matrix_from_file(self, file_name): 
+        matrix = []
+        with open(file_name) as f:
+            reader = csv.reader(f, delimiter = " ")
+            for row in reader:
+                matrix.append([float(x) for x in row])
+        return np.array(matrix)
+
+    '''
+    def get_single_bmu(self, node):
+        return SOMMapper.compute_winning_vector(node)
+    '''
+
+    '''
+    Graph functions drawn from peterwittek/somoclu
+    '''
+
+    def view_umatrix(self, matrix, bmus = None, labels = None):
+
+        #zoom = ((0, self.x_len), (0, self.y_len))
+        #figsize = (8, 8/float(zoom[1][1]/zoom[0][1]))
+        #fig = plt.figure() #figsize = figsize)
+        plt.clf()
+
+        plt.imshow(matrix) #[0:self.y_len, 0:self.x_len], aspect = "auto")
+        plt.set_cmap(cm.coolwarm)
+
+        cmap = cm.ScalarMappable(cmap=cm.coolwarm)
+        cmap.set_array(matrix)
+        plt.colorbar(cmap, orientation = "vertical", shrink = .7)
+
+        if bmus != None:
+            plt.scatter(bmus[:, 0], bmus[:, 1], c="gray")
 
 
+            for label, col, row in zip(labels, bmus[:, 0], bmus[:, 1]):
+                if label != None:
+                    plt.annotate(label, xy=(col, row), xytext=(10, -5),
+                                 textcoords='offset points', ha='left',
+                                 va='bottom',
+                                 bbox=dict(boxstyle='round,pad=0.3',
+                                           fc='white', alpha=0.8))
+    
+
+        plt.axis("off")
+        plt.savefig("u_matrix5.png")
+
+    def get_bmu(self, vector_input):
+        min_distance = 1
+        bmu = (0, 0)
+        for i, row in enumerate(self.map):
+            for j, node in enumerate(row):
+                distance = node.calculate_distance(vector_input)
+                ## Update best matching unit
+                if min_distance > distance:
+                    min_distance = distance
+                    bmu = (i, j)
+        return bmu
+
+    def get_bmus(self, vector_list):
+        bmus = []
+        for vector in vector_list:
+            bmus.append(self.get_bmu(vector))
+        return np.array(bmus)
 
 
 if __name__ == "__main__":
@@ -86,6 +155,10 @@ if __name__ == "__main__":
     #som_map = SOM(n)
     #grid = som_map.train_map(file_name)
 
-    #som_map.get_u_matrix()
+    #matrix = som_map.get_u_matrix()
+    #som_map.view_umatrix(matrix)
 
-    #UMatrix(grid)
+    #song_a = [0.9791148682, 0.4183931491,0.0484596353]
+    #song_b = [0, 0.2690661652, 0.0919894148]
+    #song_c = [0.9915464943, 0.4234423412, 0.0692342276]
+    #labels = ["song a", "song b", "song c"]
